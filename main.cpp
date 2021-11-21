b0c0a8b6b8fd7915c34da647f0523d035f11ccc4
aaronb137 <zeebra901@nevada.unr.edu>
2021-11-20 16:08:03 -0800

    added first menu for exiting and loading file in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+	
+	int choice;
+	bool wavProcess = true;
+	while (wavProcess != false) {
+		std::cout << "*******************************" << std::endl;
+		std::cout << " 1 - Load File" << std::endl;
+		std::cout << " 2 - Quit." << std::endl;
+		std::cout << " Enter your choice and press return: ";
+
+		std::cin >> choice;
+
+		switch (choice)
+		{
+			case 1:
+				std::cout << "Loading file.." << std::endl;
+				// rest of code here
+				break;
+	
+			case 2:
+				std::cout << "Exiting..." << std::endl;
+				wavProcess = false;
+				break;
+	
+			default:
+				std::cout << "Not a Valid Choice." << std::endl;
+				std::cout << "Choose again." << std::endl;
+				std::cin >> choice;
+				break;
+		}
+
+	}
+return 0;
+} 
